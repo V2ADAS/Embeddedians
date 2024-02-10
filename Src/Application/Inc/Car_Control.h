5b21e4897e6f000176d392b5de4c3cf0b53896db
Mohammed Eid Abdelmeguid Ibrahim <mohammedeidabdelmeguid@gmail.com>
2024-02-11 01:02:33 +0200

    header file of car control has been created

--- Src/Application/Inc/Car_Control.h
+++ Src/Application/Inc/Car_Control.h
@@ -1,6 +1,24 @@
-#ifndef CAR_CONTROL_
-#define CAR_CONTROL_
+#ifndef CAR_CONTROL_H
+#define CAR_CONTROL_H
 
+#include "../../HAL/HMOTOR/HMOTOR.h"
+#include "../../HAL/HSERVO/HSERVO_Int.h"
+#include "../../LIB/STD_TYPES.h"
 
+// Function to initialize car control
+void CAR_CONTROL_Init();
 
-#endif /*CAR_CONTROL_*/
+// Function to control the car movement
+void CAR_CONTROL_Move(f32 distance, s8 direction);
+
+// The "distance & direction" are paremeters that must be specified in the path tracking!
+// distance to move in cm 
+/* dirction if his target point is greater than the currect point -> send 1
+            if his target point is smaller than the currect point -> send -1 */ 
+
+// Function to control the steering angle of the car
+void CAR_CONTROL_Steer(s8 angle);
+
+// The "angle" is a parameter that must be specified in the path tracking!
+
+#endif // CAR_CONTROL_H
