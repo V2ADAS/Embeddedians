61d4469462f2170077776de04a8cd4e5bb67ac78
tokambishara <91013560+tokambishara@users.noreply.github.com>
2024-02-13 22:54:51 +0200

    Update MEXTI_Int.h

--- Src/MCAL/MEXTI/MEXTI_Int.h
+++ Src/MCAL/MEXTI/MEXTI_Int.h
@@ -1,14 +1,29 @@
-/*
- * MEXTI_Int.h
- *
- *  Created on: Nov 7, 2023
- *      Author: Hardware
- */
+/*******************************************************************************************************/
+/* Author            : Amr Elmaghraby                                                                  */
+/* Version           : V0.0.0                                                                          */
+/* Data              : 5 nov 2023                                                                      */
+/* Description       : Interface file for interfacing with the user                                    */
+/* Module  Features  :                                                                                 */
+/*      01- void SYSCFG_vConfigEXTI_Line(u8 Copy_u8Port,u8 Copy_u8EXTI_Num);                           */                                                                 
+/*      02- void MEXTI_vEnableInterrupt(u8 Copy_u8EXTI_Num);                                           */
+/*      03- void MEXTI_vDisableInterrupt(u8 Copy_u8EXTI_Num);                                          */
+/*      04- void MEXTI_vEnableEvent(u8 Copy_u8EXTI_Num);                                               */
+/*      05- void MEXTI_vDisableEvent(u8 Copy_u8EXTI_Num);                                              */
+/*      06- void MEXTI_vInterruptTrigger(u8 Copy_u8EXTI_Num,EXTI_TRIGGER	Copy_u8Trig);          */    
+/*      07- void MEXTI_vSWevent(u8 Copy_u8EXTI_Num);                                                   */
+/*      08- void MEXTI_u8ReadInterruptFlag(u8 Copy_u8EXTI_Num);                                        */
+/*******************************************************************************************************/
 
+    
+/*******************************************************************************************************/
+/*                                   guard of file will call on time in .c                             */
+/*******************************************************************************************************/
 #ifndef MCAL_MEXTI_MEXTI_INT_H_
 #define MCAL_MEXTI_MEXTI_INT_H_
 
-
+/********************************************************************************************************/
+/*                                            macros of EXTI                                            */
+/********************************************************************************************************/
 #define EXTI_PORTA		0
 #define EXTI_PORTB		1
 #define EXTI_PORTC		2
