61ef1451ac1a809d3714d3b6b462fe150de3e34d
Amr Elmaghraby <110898846+Amr-Elmaghraby@users.noreply.github.com>
2024-02-18 23:22:56 +0200

    Add files via upload

---
+++ BlackPill/Src/HAL/HLCD/HLCD_Config.h
@@ -0,0 +1,13 @@
+/*
+ * HLCD_Config.h
+ *
+ *  Created on: Feb 15, 2024
+ *      Author: Hardware
+ */
+
+#ifndef HAL_HLCD_HLCD_CONFIG_H_
+#define HAL_HLCD_HLCD_CONFIG_H_
+
+#define SLAVE_ADDRESS_LCD 0x27 // change this according to ur setup
+
+#endif /* HAL_HLCD_HLCD_CONFIG_H_ */
