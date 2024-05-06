efa6627fcc25ee79cd412d30250642c568d3f6c2
Mohamed Hawas <57467484+Mohamed-Hawas@users.noreply.github.com>
2024-05-06 11:51:19 +0300

    add calp motor

--- BlackPill/Src/HAL/HMOTOR/HMOTOR.c
+++ BlackPill/Src/HAL/HMOTOR/HMOTOR.c
@@ -1,19 +1,25 @@
 #include "HMOTOR.h"
 #include "../../LIB/BIT_MATH.h"
-#include "../../MCAL/MGPIO/MGPIO_Int.h"
+#include "../../MCAL/MGPIO/MGPIO_int.h"
 #include "../../MCAL/MGPIO/MGPIO_Config.h"
 #include "../../MCAL/MTIMER/MTIMER_Int.h"
-#include"../../MCAL/MEXTI/MEXTI_int.h"
+#include"../../MCAL/MEXTI/MEXTI_Int.h"
 #include"../../MCAL/MNVIC/MNVIC_int.h"
-#include"../../MCAL/MSTK/MSYSTICK_int.h"
+#include"../../MCAL/MSTK/MSYSTICK_Int.h"
 #include"../../APP/Inc/Motion_Planing.h"
-#define WHEEL_AREA   26
-#define PULSES_PER_REVOLUTION 1024
 
-volatile MOTOR_PINS_t Motor_Pins ;
-volatile u32 encoder_pulses = 0 ;
-volatile u32 moved_distance_pulses = 0 ;
-volatile u32 target_distance_pulses = 0 ;
+MOTOR_t DC_Motor ;
+MOTOR_t CALP_Motor ;
+
+ENCODER_t Encoder ;
+u32 encoder_pulses = 0 ;
+f32 moved_distance = 0 ;
+f32 total_moved_distance = 0 ;
+
+/*****************************Local Functions***********************************/
+MOTOR_t* LOC_GetMotorName(u8 MOTOR);
+void LOC_MotorCallBack();
+
 
 //void Increment_Pulse_CallBack(){
 //	encoder_pulses++ ;
@@ -31,48 +37,83 @@ volatile u32 target_distance_pulses = 0 ;
 
 
 
-void HAL_MOTOR_Init(u8 PORT_N1 , u8 PIN_N1, u8 PORT_N2 , u8 PIN_N2 , Enum_TIMER_NUM PWM_TIMER , Enum_TIMER_CHs PWM_CH){
-	Motor_Pins.PORT_N1  = PORT_N1 	;
-	Motor_Pins.PORT_N2  = PORT_N2 	;
-	Motor_Pins.PWM_TIMER = PWM_TIMER  ;
-	Motor_Pins.PIN_N1 	 = PIN_N1	;
-	Motor_Pins.PIN_N2 	 = PIN_N2	;
-	Motor_Pins.PWM_CH  = PWM_CH	;
+void HAL_MOTOR_Init(u8 MOTOR , u8 PORT_N1 , u8 PIN_N1, u8 PORT_N2 , u8 PIN_N2 , Enum_TIMER_NUM PWM_TIMER , Enum_TIMER_CHs PWM_CH){
+	MOTOR_t *Motor = LOC_GetMotorName(MOTOR);
+
+	Motor->PORT_N1  = PORT_N1 	;
+	Motor->PORT_N2  = PORT_N2 	;
+	Motor->PWM_TIMER = PWM_TIMER;
+	Motor->PIN_N1 	 = PIN_N1	;
+	Motor->PIN_N2 	 = PIN_N2	;
+	Motor->PWM_CH  = PWM_CH	;
 	MGPIO_vSetPinMode(PORT_N1, PIN_N1, OUTPUT);
 	MGPIO_vSetPinMode(PORT_N2, PIN_N2, OUTPUT);
 }
 
 void HAL_ENCODER_Init( Enum_TIMER_NUM CNTR_TIMER , Enum_TIMER_CHs CNTR_CH ){
-	Motor_Pins.CNTR_TIMER = CNTR_TIMER ;
-	Motor_Pins.CNTR_CH = CNTR_CH ;
+	Encoder.CNTR_TIMER = CNTR_TIMER ;
+	Encoder.CNTR_CH = CNTR_CH ;
 	MNVIC_vEnableInterrupt(NVIC_TIM5);
 }
 
-void HAL_MOTOR_MOVE(u8 DIRCTION ,u8 SPEED , f32 DISTANCE_cm_){
+void HAL_MOTOR_MOVE(u8 MOTOR , u8 DIRCTION ,u8 SPEED , f32 DISTANCE_cm_){
+	MOTOR_t *Motor = LOC_GetMotorName(MOTOR);
+
 	f32 high_duty = (SPEED / 100.0)  ;
 	f32 num_of_revolutions  ;
-	moved_distance_pulses = 0 ;
-	target_distance_pulses = 0 ;
+	f32 moved_distance_pulses = 0 ;
+	u32 target_distance_pulses = 0 ;
 	switch(DIRCTION){
 	case BACKWARD :
-		MGPIO_vSetPinValue(Motor_Pins.PORT_N1, Motor_Pins.PIN_N1, HIGH);
-		MGPIO_vSetPinValue(Motor_Pins.PORT_N2, Motor_Pins.PIN_N2, LOW);
+		MGPIO_vSetPinValue(Motor->PORT_N1, Motor->PIN_N1, HIGH);
+		MGPIO_vSetPinValue(Motor->PORT_N2, Motor->PIN_N2, LOW);
 		break;
 	case FORWARD:
-		MGPIO_vSetPinValue(Motor_Pins.PORT_N1, Motor_Pins.PIN_N1, LOW);
-		MGPIO_vSetPinValue(Motor_Pins.PORT_N2, Motor_Pins.PIN_N2, HIGH);
+		MGPIO_vSetPinValue(Motor->PORT_N1, Motor->PIN_N1, LOW);
+		MGPIO_vSetPinValue(Motor->PORT_N2, Motor->PIN_N2, HIGH);
 		break;
 	}
 	/****************************speed********************************/
-	MTIMER_vPWM(Motor_Pins.PWM_TIMER, Motor_Pins.PWM_CH, 10000, high_duty*10000);
+	MTIMER_vPWM(Motor->PWM_TIMER, Motor->PWM_CH, 10000, high_duty*10000);
 	/***************************distance******************************/
 	num_of_revolutions = (DISTANCE_cm_/ WHEEL_AREA);
 	target_distance_pulses = (u32)(num_of_revolutions * PULSES_PER_REVOLUTION);
-	MTIMER_vEXTCNTClock(Motor_Pins.CNTR_TIMER, Motor_Pins.CNTR_CH , target_distance_pulses );
-	MTIMER_CallBack(Motor_Pins.CNTR_TIMER,HAL_MOTOR_STOP );
+	MTIMER_vEXTCNTClock(Encoder.CNTR_TIMER, Encoder.CNTR_CH , target_distance_pulses );
+	moved_distance = DISTANCE_cm_ ;
+	MTIMER_CallBack(Encoder.CNTR_TIMER,LOC_MotorCallBack );
 }
 
-void HAL_MOTOR_STOP(){
-	MGPIO_vSetPinValue(Motor_Pins.PORT_N1, Motor_Pins.PIN_N1, LOW);
-	MGPIO_vSetPinValue(Motor_Pins.PORT_N2, Motor_Pins.PIN_N2, LOW);
+void HAL_MOTOR_STOP(u8 MOTOR){
+	MOTOR_t *Motor = LOC_GetMotorName(MOTOR);
+
+	MGPIO_vSetPinValue(Motor->PORT_N1, Motor->PIN_N1, LOW);
+	MGPIO_vSetPinValue(Motor->PORT_N2, Motor->PIN_N2, LOW);
 }
+
+
+f32 HAL_MOTOR_GetMovedDistance(){
+	u32 moved_distance_pulses = MTIMER_u32GetElapsedTicks(Encoder.CNTR_TIMER);
+	f32 num_of_revolutions = (f32) moved_distance_pulses / PULSES_PER_REVOLUTION ;
+	f32 moved_distance_cm = num_of_revolutions * WHEEL_AREA ;
+	return total_moved_distance + moved_distance_cm ;
+}
+void LOC_MotorCallBack(){
+	HAL_MOTOR_STOP(DC_MOTOR);
+	total_moved_distance += moved_distance ;
+}
+
+MOTOR_t* LOC_GetMotorName(u8 MOTOR){
+	MOTOR_t *Motor ;
+	switch (MOTOR) {
+	case DC_MOTOR:
+		Motor = &DC_Motor ;
+		break;
+	case CALP_MOTOR:
+		Motor = &CALP_Motor ;
+		break;
+	default:
+		break;
+	}
+	return Motor ;
+}
+
