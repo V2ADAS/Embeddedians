4fcc7470ffe87a206aa71c0222bd4d129a11f5dd
OMAR WAEL <omarwael012@gmail.com>
2024-07-26 02:16:04 +0300

    fix in dc and servo

--- BlackPill/Src/HAL/HMOTOR/HMOTOR.c
+++ BlackPill/Src/HAL/HMOTOR/HMOTOR.c
@@ -97,6 +97,8 @@ void HAL_MOTOR_ForceStop(u8 MOTOR){
 
 	MGPIO_vSetPinValue(Motor->PORT_N1, Motor->PIN_N1, LOW);
 	MGPIO_vSetPinValue(Motor->PORT_N2, Motor->PIN_N2, LOW);
+
+	setSpeed(0);
 }
 
 void HAL_MOTOR_StopDcAfterDistance(f32 DISTANCE_cm_){
